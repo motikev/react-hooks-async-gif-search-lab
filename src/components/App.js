52fb7a9d30f436d4bd55b919054ef6af4b65ad67
motikev <kmotties@gmail.com>
2023-07-29 21:09:03 +0300

    Done Editing

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,15 @@
-// GifSearch.js
-import React, { useState } from "react";
+// App.js
+import React from "react";
+import NavBar from "./NavBar";
+import GifListContainer from "./GifListContainer";
 
-const GifSearch = ({ onSearch }) => {
-  const [query, setQuery] = useState("");
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    onSearch(query);
-  };
-
+const App = () => {
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Search for GIFs..."
-      />
-      <button type="submit">Search</button>
-    </form>
+    <div>
+      <NavBar />
+      <GifListContainer />
+    </div>
   );
 };
 
-export default GifSearch;
+export default App;
--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,4 +1,3 @@
-// GifListContainer.js
 import React, { useState } from "react";
 import GifList from "./GifList";
 import GifSearch from "./GifSearch";
@@ -7,11 +6,17 @@ const GifListContainer = () => {
   const [gifs, setGifs] = useState([]);
 
   const handleSearch = (query) => {
-    const apiKey = "LNI1rLNI1rqiXniMM9vTbDpwLCfBlnIQsWqcK";
+    // Replace 'YOUR_API_KEY' with your actual Giphy API key
+    const apiKey = "Bkc6e5NAh5ft8gbOhxHUb3Tr6sFd0Fgy";
     const apiUrl = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=${apiKey}&limit=3`;
 
     fetch(apiUrl)
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
       .then((data) => {
         const gifData = data.data.map((gif) => gif.images.original.url);
         setGifs(gifData);
